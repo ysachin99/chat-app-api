144f9dbd4c72cceb98f92fe652119e238a13df02
ysachin99 <visitosachin97@gmail.com>
2023-08-01 20:41:46 +0530

    initial api commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET','POST'],
+    },
+});
+
+io.on('connection', (socket)=>{
+    console.log(`User Connected : ${socket.id}`)
+
+    socket.on('join_group', (data)=>{
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined group: ${data}`);
+    });
+
+    socket.on('send_message', (data)=>{
+        socket.to(data.room).emit('recieve_message',data);
+    });
+
+    socket.on('disconnect',()=>{
+        console.log('user Disconnected', socket.id);
+    });
+})
+
+
+server.listen(4001,()=>{
+    console.log('server is running...');
+})
